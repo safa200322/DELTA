01363454559f473f542ff5d00631be1e8bbab9f0
i0mvrr <omar.alyousufi10@gmail.com>
2025-05-04 19:12:17 +0300

    Update chauffeurRoutes.js

--- src/routes/chauffeurRoutes.js
+++ src/routes/chauffeurRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const chauffeurController = require("../controllers/chauffeurController");
-const adminAuth = require("../middleware/adminAuth"); //  protect route
+const adminAuth = require("../middleware/adminAuth"); // protect route
 
 // Add new chauffeur (admin only)
 router.post("/", adminAuth, chauffeurController.addChauffeur);
 
 // Auto-assign chauffeur to a reservation
-router.post("/assign/:reservationId", adminAuth, chauffeurController.assignChauffeur);
+router.post("/assign", adminAuth, chauffeurController.assignChauffeur);
 
 // Update chauffeur status (approve/reject)
 router.put("/chauffeurs/:id/status", adminAuth, chauffeurController.updateChauffeurStatus);
+
 module.exports = router;
