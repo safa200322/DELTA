9401e90eb66ffafeba2a6ce0980e98ee8695e0b9
safa200322 <sadd4a253@gmail.com>
2025-06-02 03:04:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const bodyParser = require("body-parser");
 const session = require("express-session");
@@ -14,7 +15,6 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }       // Set to true only if using HTTPS
 }));
-
 // Route Imports
 const authRoutes = require("./src/routes/authRoutes");
 const vehicleRoutes = require("./src/routes/vehicleRoutes");
@@ -23,6 +23,10 @@ const chauffeurRoutes = require("./src/routes/chauffeurRoutes");
 const paymentRoutes = require("./src/routes/paymentRoutes");
 const reservationRoutes = require("./src/routes/reservationRoutes");
 const filterRoutes = require('./src/routes/filterRoutes');
+const boatFilterRoutes = require('./src/routes/boatFilterRoutes');
+const bicycleFilterRoutes = require("./src/routes/bicycleFilterRoutes");
+const motorcycleFilterRoutes = require('./src/routes/motorcycleFilterRoutes');
+
 
 // Routes
 app.use("/api", searchRoutes);
@@ -31,14 +35,20 @@ app.use("/api/vehicles", vehicleRoutes);
 app.use("/api/chauffeurs", chauffeurRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/reservations", reservationRoutes);
+app.use("/api/accessories", require("./src/routes/accessoryRoutes"));
+app.use("/api/admin", require("./src/routes/dashboardRoutes"));
 app.use('/api/vehicles', filterRoutes);
+app.use('/api/boats', boatFilterRoutes);
+app.use('/api/bicycles', bicycleFilterRoutes);
+app.use('/api/motorcycles', motorcycleFilterRoutes);
+
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Vehicle Rental System API ');
+    res.send('Welcome to the Vehicle Rental System API 🚀');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(` Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
