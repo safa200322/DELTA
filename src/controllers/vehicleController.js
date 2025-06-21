442c361168c9d6f6bc6c893f222e866fe2a41aa4
safa200322 <sadd4a253@gmail.com>
2025-06-21 18:32:36 +0300

    Update vehicleController.js

--- src/controllers/vehicleController.js
+++ src/controllers/vehicleController.js
@@ -1,106 +1,37 @@
-const vehicleModel = require('../models/vehicleModel');
-const carModel = require('../models/carModel');
-const boatModel = require('../models/boatModel');
-const bicycleModel = require('../models/bicycleModel');
-const motorcycleModel = require('../models/motorcycleModel');
+const express = require("express");
+const vehicleController = require("../controllers/vehicleController");
+const adminAuth = require("../middleware/adminAuth"); 
+const authenticateToken = require('../middleware/authMiddleware');
+const router = express.Router();
 
-exports.addCar = async (req, res) => {
-  const { Brand, Model, Year, FuelType, Seats, Color, Transmission, Location } = req.body;
-
-  if (!Brand || !Model || !Year || !FuelType || !Seats || !Location) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
-
-  try {
-    const [vehicleResult] = await vehicleModel.insertVehicle('Car', Location);
-    const vehicleId = vehicleResult.insertId;
-
-    await carModel.insertCar(vehicleId, { Brand, Model, Year, FuelType, Seats, Color, Transmission });
-
-    res.status(201).json({ message: "Car and vehicle added successfully", VehicleID: vehicleId });
-  } catch (err) {
-    console.error("Error adding car:", err);
-    res.status(500).json({ message: "Server error", error: err });
-  }
-};
+//router.post("/", adminAuth, vehicleController.addVehicle);
 
 
-exports.addBoat = async (req, res) => {
-  const { Capacity, EngineType, Brand, BoatType, Location } = req.body;
+//router.get("/", authenticateToken, vehicleController.getAllVehicles);
+//router.get("/cars", vehicleController.getAllCars);
+//router.get("/boats", vehicleController.getAllBoats);
+//router.get("/bicycles", vehicleController.getAllBicycles);
+//router.get("/motorcycles", vehicleController.getAllMotorcycles);
+//router.get("/available", vehicleController.getAvailableVehicles);
+//router.get("/:id", vehicleController.getVehicleById);
 
-  if (!Capacity || !EngineType || !Brand || !BoatType || !Location) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
+router.post("/cars", vehicleController.addCar);
+router.post("/boats", vehicleController.addBoat);
+router.post("/bicycles", vehicleController.addBicycle);
+router.post("/motorcycles", vehicleController.addMotorcycle);
 
-  try {
-    const [vehicleResult] = await vehicleModel.insertVehicle('boats', Location);
-    const vehicleId = vehicleResult.insertId;
 
-    await boatModel.insertBoat(vehicleId, { Capacity, EngineType, Brand, BoatType });
+router.patch('/:id/approve', adminAuth, vehicleController.approveVehicle);
+router.patch('/:id/reject', adminAuth,vehicleController.rejectVehicle);
 
-    res.status(201).json({ message: "Boat and vehicle added successfully", VehicleID: vehicleId });
-  } catch (err) {
-    console.error("Error adding boat:", err);
-    res.status(500).json({ message: "Server error", error: err });
-  }
-};
+//router.put("/:id", adminAuth, vehicleController.updateVehicle);
+//router.delete("/:id", adminAuth, vehicleController.deleteVehicle);
 
-exports.addBicycle = async (req, res) => {
-  const { Type, Gears, Location } = req.body;
 
-  if (!Type || !Gears || !Location) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
 
-  try {
-    const [vehicleResult] = await vehicleModel.insertVehicle('Bicycle', Location);
-    const vehicleId = vehicleResult.insertId;
+router.get("/test", (req, res) => {
+  res.send(" API is working!");
+});
 
-    await bicycleModel.insertBicycle(vehicleId, { Type, Gears });
 
-    res.status(201).json({ message: "Bicycle and vehicle added successfully", VehicleID: vehicleId });
-  } catch (err) {
-    console.error("Error adding bicycle:", err);
-    res.status(500).json({ message: "Server error", error: err });
-  }
-};
-
-exports.addMotorcycle = async (req, res) => {
-  const { Brand, Engine, Year, Type, Location, color } = req.body;
-
-  if (!Brand || !Engine || !Year || !Type || !Location || !color) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
-
-  try {
-    const [vehicleResult] = await vehicleModel.insertVehicle('Motorcycle', Location);
-    const vehicleId = vehicleResult.insertId;
-
-    await motorcycleModel.insertMotorcycle(vehicleId, { Brand, Engine, Year, Type, color });
-
-    res.status(201).json({ message: " added sucessfully", VehicleID: vehicleId });
-  } catch (err) {
-    console.error("error adding:", err);
-    res.status(500).json({ message: "Server error", error: err });
-  }
-};
-
-exports.approveVehicle = async (req, res) => {
-  try {
-    const vehicleID = req.params.id;
-    await vehicleModel.updateVehicleStatus(vehicleID, 'approved');
-    res.status(200).json({ message: 'Vehicle approved successfully' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.rejectVehicle = async (req, res) => {
-  try {
-    const vehicleID = req.params.id;
-    await vehicleModel.updateVehicleStatus(vehicleID, 'rejected');
-    res.status(200).json({ message: 'Vehicle rejected successfully' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+module.exports = router;
