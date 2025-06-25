01ca0c1d7e999a40e91adafab2c90ce1a78f6917
calebsakala <108591422+calebsakala@users.noreply.github.com>
2025-06-25 17:45:37 +0300

    fix: Update server port to 5000 and adjust .gitignore for misc files

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Vehicle Rental System API ');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(` Server running on http://localhost:${PORT}`);
 });
