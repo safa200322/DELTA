f88db650405980739fdfeab229c5bde36a5141db
safa200322 <sadd4a253@gmail.com>
2025-06-25 08:25:49 +0300

    Update chauffeurRoutes.js

--- src/routes/chauffeurRoutes.js
+++ src/routes/chauffeurRoutes.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const chauffeurController = require("../controllers/chauffeurController");
-const adminAuth = require("../middleware/adminAuth"); //  protect route
+const adminAuth = require("../middleware/adminAuth"); 
+const authMiddleware = require('../middleware/authMiddleware');
+const authChauffeur = require('../middleware/authChauffeur');
 
-router.post("/chauffeurs", chauffeurController.addChauffeur);
+
+router.post("/chauffeurs/register", chauffeurController.registerChauffeur);
+
+router.post("/chauffeurs/login", chauffeurController.loginChauffeur);
 
 router.patch("/chauffeurs/approve/:id",adminAuth, chauffeurController.approveChauffeur);
+
 router.patch("/chauffeurs/reject/:id",adminAuth, chauffeurController.rejectChauffeur);
 
-router.patch("/assignments/respond/:reservationId", chauffeurController.respondToAssignment);
+router.patch("/assignments/respond/:reservationId",authChauffeur, chauffeurController.respondToAssignment);
 
-router.post("/assignments/assign/:reservationId", chauffeurController.assignChauffeur);
+router.post("/assignments/assign/:reservationId", authMiddleware, chauffeurController.assignChauffeur);
 
-router.get("/chauffeurs/pending", chauffeurController.getPendingChauffeurs);
-router.get('/:chauffeurId/pending-assignments', chauffeurController.getPendingAssignments);
-router.get("/assignments/pending/:chauffeurId", chauffeurController.getPendingAssignments);
+router.get("/chauffeurs/pending", adminAuth, chauffeurController.getPendingChauffeurs);
+
+router.get("/assignments/pending/:chauffeurId", authChauffeur, chauffeurController.getPendingAssignments);
 
 module.exports = router;
