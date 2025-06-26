3a9743cc486e4f52f96a229a5c484046826c782a
calebsakala <108591422+calebsakala@users.noreply.github.com>
2025-06-26 18:54:12 +0300

    chore: Remove redundant migration files for dropping check constraint and LicenseID column from Reservation table

--- backend/migrations/015_drop_chk_and_licenseid_from_reservation.sql
+++
@@ -1,3 +0,0 @@
--- Migration: Drop check constraint and LicenseID column from Reservation table
-ALTER TABLE Reservation DROP CHECK reservation_chk_1;
-ALTER TABLE Reservation DROP COLUMN LicenseID;
